d2915250fd8ed174ed7e042bf35a6cda6f97857c
mariasantizo <maria.santizo@opendeusto.es>
2022-04-06 09:46:17 +0200

    prueba

--- main.c
+++ main.c
@@ -24,7 +24,7 @@ int main(void) {
     //MENU
     int opcion;
     char opcionChar;
-    int opcionCuenta
+    int opcionCuenta;
     while (opcionChar!='q') {
         imprimirMenu();
         scanf("%i", opcion);
