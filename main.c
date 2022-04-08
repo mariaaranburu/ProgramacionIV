db51f24777b8105998800a966ebddb98d0adcb34
mariasantizo <maria.santizo@opendeusto.es>
2022-04-08 13:58:39 +0200

    main

--- main.c
+++ main.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "cliente.h"
 #include "cuentacorriente.h"
 #include "transaccion.h"
 #define MAX_CLIENTES 10
 #define MAX_CHAR 30
+
+void clearIfNeeded(char *str, int max_line)
+{
+	// Limpia los caracteres de más introducidos
+	if ((strlen(str) == max_line-1) && (str[max_line-2] != '\n'))
+		while (getchar() != '\n');
+}
 
 void iniciarSesion(void);
 void imprimirMenu(void);
@@ -19,44 +27,44 @@ int main(void) {
     clientes[0].edad = 20;
     char str[10];
     //INICIAR SESIÓN
-    printf("      INICIO DE SESION\n");
-    printf("-----------------------------\n");
-    printf("Inserta tu nombre: \n");
-    char* nombre;
-    scanf("%s", nombre);
-    //*nombre=*str;
-    fflush(stdin);
-    printf("-----------------------------\n");
-    printf("Inserta tu contrasenya: \n");
-    char* contrasenya;
-    scanf("%s", contrasenya);
-    fflush(stdin);
-    //*contrasenya=*str;
-    printf("Has introducido: %s, %s\n", nombre, contrasenya);
-    
-    //COMPROBACION DE INICIO DE SESION: FOR, IF
     int encontrado = -1;
-    for (int i=0; i<MAX_CLIENTES; i++) {
-        if (clientes[i].nombre == nombre && clientes[i].contrasenya == contrasenya) {
-            encontrado = 1;
-        }
-    }
-    if (encontrado == -1) {
-        printf("El usuario y la contrasenya no son correctos. Prueba otra vez.\n");
-    } else {
-        //MENU
-        int opcion;
-        char opcionChar;
-        int opcionCuenta;
-        while (opcionChar!='q') {
-            imprimirMenu();
-            scanf("%i", opcion);
-            scanf("%c", opcionChar);
-            if (opcion == 1) {
-                imprimirMenuCuenta();
+    while (encontrado != 1) {
+        printf("      INICIO DE SESION\n");
+        printf("-----------------------------\n");
+        printf("Inserta tu nombre: \n");
+        char* nombre = (char*) malloc (MAX_CHAR*sizeof(char));
+        fgets(nombre, MAX_CHAR, stdin);
+        sscanf(nombre, "%s");
+        clearIfNeeded(nombre, MAX_CHAR);
+        printf("-----------------------------\n");
+        printf("Inserta tu contrasenya: \n");
+        char* contrasenya = (char*) malloc (MAX_CHAR*sizeof(char));
+        fgets(contrasenya, MAX_CHAR, stdin);
+        sscanf(contrasenya, "%s");
+        clearIfNeeded(contrasenya, MAX_CHAR);
+        printf("%s, %s\n", nombre, contrasenya);
+        for (int i=0; i<MAX_CLIENTES; i++) {
+            if (clientes[i].nombre == nombre && clientes[i].contrasenya == contrasenya) {
+                encontrado = 1;
             }
         }
+        if (encontrado == -1) {
+            printf("El usuario y la contrasenya no son correctos. Prueba otra vez.\n");
+        }
     }
+    //MENU
+    int opcion;
+    char opcionChar;
+    int opcionCuenta;
+    while (opcionChar!='q') {
+        imprimirMenu();
+        scanf("%i", &opcion);
+        scanf("%c", &opcionChar);
+        if (opcion == 1) {
+            imprimirMenuCuenta();
+        }
+    }
+    
     return 0;
 }
 
