2b566ca99083d1c3c8e58d1641457fb5f85d9f00
María Aramburu García <maria.aramburu@opendeusto.es>
2022-04-11 20:50:03 +0200

    Jerarquía

--- accion.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "accion.h"
-
-void imprimirAccion(Accion a){
-    printf("[Numero: %i, Precio actual: %f, Precio compra: %f, Empresa: %s]\n",a.num_valor,a.prec_actual,a.prec_compra, a.empresa);
-}
--- transaccion.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "transaccion.h"
-#include "cuentacorriente.h"
-
-void imprimirTransaccion(Transaccion t){
-    printf("[num: %i; importe: %f; desc: %s; origen: %i; destino: %i]",t.num,t.importe,t.descripcion,t.origen->numero, t.destino->numero);
-}
