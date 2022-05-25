5efa12a2b85664232848ba2e98e4ec2757968219
marinaojinaga <marinaojinaga@opendeusto.es>
2022-05-25 16:58:41 +0200

    borrado

--- logicaDeNegocio/transaccion.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "transaccion.h"
-#include "cuentacorriente.h"
-
-void imprimirTransaccion(Transaccion t){
-    printf("[num: %i; importe: %.2f; desc: %s; origen: %i; destino: %i]\n",t.num,t.importe,t.descripcion,t.origen->numero, t.destino->numero);
-}
