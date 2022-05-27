c6918da340852b9918db4b6f67bacc0e5b682433
mariasantizo <maria.santizo@opendeusto.es>
2022-05-27 10:29:20 +0200

    cambioNombres

---
+++ c++/tarjeta_.h
@@ -0,0 +1,37 @@
+#ifndef TARJETA_H_
+#define TARJETA_H_
+
+//namespace c++
+class Tarjeta
+{
+	private:
+		int num;
+	    int pin;
+	    int limite;
+	    char* tipo;
+	    int numCC;
+
+	public:
+		//constructor por defecto
+		Tarjeta(); 
+		//constructor con parametros
+		Tarjeta(int num, int pin, int limite, char* tipo, int numCC);
+		//destructor
+		~Tarjeta();
+
+		void setNum(int num);
+		int getNum() const;
+		void setPin(int pin);
+		int getPin() const;
+		void setLimite(int limite);
+		int getLimite() const;
+		void setTipo(char* tipo);
+		char* getTipo() const;
+		void setNumCC(int numCC);
+		int getNumCC() const;
+
+		void imprimirInfoTarjeta(Tarjeta t);
+		void imprimirNumTarjeta(Tarjeta* t, int numTarjetas);
+};
+
+#endif
---
+++ c++/transaccion_.h
@@ -0,0 +1,39 @@
+#include "cuentacorriente.h"
+#ifndef TRANSACCION_H_
+#define TRANSACCION_H_
+
+//namespace c++
+class Transaccion
+{
+	private:
+		int num;
+	    float importe;
+	    char* descripcion;
+	    CuentaCorriente* origen;
+	    CuentaCorriente* destino;
+
+	public:
+		//constructor por defecto
+		Transaccion(); 
+		//constructor con parametros
+		Transaccion(int num, float importe, char* descripcion, CuentaCorriente* origen, CuentaCorriente* destino);
+		//destructor
+		~Transaccion();
+
+		void setNum(int num);
+		int getNum() const;
+		void setImporte(float importe);
+		float getImporte() const;
+		void setDescripcion(char* descripcion);
+		char* getDescripcion() const;
+		void setOrigen(CuentaCorriente* origen);
+		CuentaCorriente* getOrigen() const;
+		void setDestino(CuentaCorriente* destino);
+		CuentaCorriente* getDestino() const;
+
+		void imprimirTransaccion(Transaccion t);
+		
+void Transaccion::hacerTransaccion(CuentaCorriente o, CuentaCorriente d, float importe);
+};
+
+#endif
